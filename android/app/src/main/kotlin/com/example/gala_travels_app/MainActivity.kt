1899e72088fad376bf2b987f670ddd62d68d2ec4
Prathamesh Devkate <80754725+Pradev18@users.noreply.github.com>
2026-02-13 15:05:56 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/gala_travels_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.gala_travels_app
+
+import io.flutter.embedding.android.FlutterFragmentActivity // <-- Add this import
+
+class MainActivity: FlutterFragmentActivity() { // <-- Change FlutterActivity to FlutterFragmentActivity
+    // ... existing code ...
+}
